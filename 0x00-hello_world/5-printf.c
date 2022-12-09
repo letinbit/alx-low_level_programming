8cffc7f670527a524e067057fff9000ec19903d5
letinbit <letinbit@gmail.com>
2022-12-08 16:17:17 -0800

    My first C code with proper Betty style

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* To test printf() function */
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * This program wipp print what is written in printf function with newline
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
