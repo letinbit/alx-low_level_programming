c4b9c9f2e34bb74fb7c4561557940fb6923b440d
letinbit <letinbit@gmail.com>
2022-12-09 13:00:52 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+/* alphABET*/
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *Print both lower and upper case alphabets
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lower = 97;
+	int upper = 65;
+
+	for (; lower <= 122; lower++)
+		putchar(lower);
+	for (; upper <= 90; upper++)
+		putchar(upper);
+	printf("\n");
+	return (0);
+}
