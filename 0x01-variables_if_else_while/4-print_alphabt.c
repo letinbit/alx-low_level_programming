2999f51a9ed5590e10bdf1f6fc7aa431193554ac
letinbit <letinbit@gmail.com>
2022-12-09 13:21:32 -0800

    Excluding only e and q from the alphabete

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/* Use putchar() function to print alphabets */
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alph = 97;
+
+	while (alph <= 122)
+	{
+		if (alph == 101)
+			alph++;
+		else if (alph == 113)
+			alph++;
+		else
+			putchar(alph);
+		alph++;
+	}
+	printf("\n");
+	return (0);
+}
