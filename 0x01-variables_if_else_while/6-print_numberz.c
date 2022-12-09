da93e8cce5e8b1b1d9858a6e18cf26bcc34da136
letinbit <letinbit@gmail.com>
2022-12-09 13:54:39 -0800

    Print 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/* Printing the figuers of base 10*/
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * This code will prinnt numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decimal = 0;
+
+	do {
+		if (decimal / 2 == 5)
+			break;
+		printf("%d", decimal);
+		decimal++;
+	} while (decimal);
+	printf("\n");
+	return (0);
+}
