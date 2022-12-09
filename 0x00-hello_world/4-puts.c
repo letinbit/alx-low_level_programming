0cf2741e7343a5e4b79835a95c67a40fbb8531bc
letinbit <letinbit@gmail.com>
2022-12-08 16:51:51 -0800

    Using function puts() to print variables value

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*To check puts() function*/
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * This program will print the velue of the variable using puts()
+ * function and then add newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char Text[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(Text);
+	return (0);
+}
