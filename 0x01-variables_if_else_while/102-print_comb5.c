7277f7c95fe48fc33f83e358d65ff51d62a85dd8
letinbit <letinbit@gmail.com>
2022-12-10 06:45:14 -0800

    Display two digit integer number using putchar() function.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+/*        */
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * putchar() function can print int less than 10. so we need to use division and modulo 
+ * to print int a = 23 putchar(a/10 + '0') putchar(a%10 + '0')
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstcomb;
+	int secondcomb;
+
+	for (firstcomb = 0; firstcomb <= 99; firstcomb++)
+	{
+		for (secondcomb = firstcomb + 1; secondcomb <= 99; secondcomb++)
+		{
+			putchar(firstcomb / 10 + '0');
+			putchar(firstcomb % 10 + '0');
+			
+			putchar(' ');
+			putchar(secondcomb / 10 + '0');
+			putchar(secondcomb % 10 + '0');
+		
+			if (firstcomb == 98)
+				if (secondcomb ==99)
+					break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
