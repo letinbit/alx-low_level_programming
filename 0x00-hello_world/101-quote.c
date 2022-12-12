876cf9f944fcc2b2e65484066517565358dcf70b
letinbit <letinbit@gmail.com>
2022-12-11 20:41:04 -0800

    Print to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int i, j;
 	char phrase_1[32] = "and that piece of art is useful";
-	char phrase_2[26] = " - Dora Korpar, 2015-10-19";
+	char phrase_2[27] = " - Dora Korpar, 2015-10-19\n";
 
 	for (i = 0; i < 32; i++)
 		putchar(phrase_1[i]);
 	putchar('"');
-	for (j = 0; j < 27; j++)
+	for (j = 0; j < 28; j++)
 		putchar(phrase_2[j]);
-	putchar('\n');
+	/*putchar('\n');*/
 	return (1);
 }
