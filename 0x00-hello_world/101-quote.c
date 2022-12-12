514d605a99c86fb9f1b1c16d5f0b622cb18de667
letinbit <letinbit@gmail.com>
2022-12-12 05:35:27 -0800

    Printing text using function other than functions listed in the NAME section of the man (3) printf or man (3) puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 /*Author: Neway Solomon */
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main-Entry point
  * This code will print a combination of letters
@@ -9,15 +9,7 @@
  */
 int main(void)
 {
-	int i, j;
-	char phrase_1[32] = "and that piece of art is useful";
-	char phrase_2[27] = " - Dora Korpar, 2015-10-19\n";
-
-	for (i = 0; i < 32; i++)
-		putchar(phrase_1[i]);
-	putchar('"');
-	for (j = 0; j < 28; j++)
-		putchar(phrase_2[j]);
-	/*putchar('\n');*/
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
