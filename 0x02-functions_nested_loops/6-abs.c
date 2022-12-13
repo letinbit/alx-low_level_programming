98707f249542db3c3da945bb249506e7a60fd711
letinbit <letinbit@gmail.com>
2022-12-13 08:32:00 -0800

    Function that calculate the ablosute value a number using the abs() built-in function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/*Author is Neway Solomon*/
+#include <stdlib.h>
+
+/**
+ * _abs - is a function that uses abs() function to return the
+ * absolute value of passed argument.
+ * @n: is a parameter that its absolute vale will be calculated
+ * Return: the return value of abs()
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
