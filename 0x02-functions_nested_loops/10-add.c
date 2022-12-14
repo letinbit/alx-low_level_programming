cfc8ab76b828f81429fa6fc66c895afccc4c366d
letinbit <letinbit@gmail.com>
2022-12-13 17:56:20 -0800

    Function that calculater and return the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/* Author is NewaySolomon */
+
+/**
+ * add - is a function that add tow integer numbers
+ * @a: the firt argument of function add()
+ * @b: the second argument of function add()
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
