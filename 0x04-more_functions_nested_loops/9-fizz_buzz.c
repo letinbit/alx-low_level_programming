84859d1fe5ca4d144bd1d05ad26c2058d1a8ebeb
letinbit <letinbit@gmail.com>
2022-12-15 21:41:02 +0300

    Fizz Buzz program 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,14 +18,25 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("%s", FizzBuzz);
+		}
 		else if (i % 3 == 0)
+		{
 			printf("%s", Fizz);
+		}
 		else if (i % 5 == 0)
+		{
 			printf("%s", Buzz);
+		}
 		else
+		{
 			printf("%d", i);
-		printf(" ");
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
