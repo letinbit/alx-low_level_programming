39153e7c799e650b27140f9545a5857d9106eb60
letinbit <letinbit@gmail.com>
2022-12-15 18:52:27 +0300

    Function that print 0 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,21 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, x, z;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 			if (j > 9)
 			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
+				for (x = 1; x <= 2; x++)
+				{
+					if (x == 1)
+						z = j / 10;
+					else
+						z = j % 10;
+					_putchar(z + '0');
+				}
 			}
 			else
 				_putchar(j + '0');
