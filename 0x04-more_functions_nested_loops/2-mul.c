588a0b5897b94433688f1bfc828939baecf60a2f
letinbit <letinbit@gmail.com>
2022-12-15 12:17:03 +0300

    Function that return the multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/* Author is Neway Solomon */
+
+/**
+ * mul - is a function thar returns the multiplication of two numbers
+ * @a: the first opperand for the multiplication
+ * @b: the second operand for the multiplication
+ * Return: aXb
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
