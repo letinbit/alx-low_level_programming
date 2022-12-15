c35a065ce784c345170f49b25193b3f600cf8f7f
letinbit <letinbit@gmail.com>
2022-12-15 19:05:28 +0300

    Function that print 0 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,6 +13,7 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
+		{
 			if (j > 9)
 			{
 				for (x = 1; x <= 2; x++)
@@ -26,6 +27,7 @@ void more_numbers(void)
 			}
 			else
 				_putchar(j + '0');
+		}
 		_putchar('\n');
 	}
 }
