c2e035f99033b2d60e5a49de8c759d8dae497f3a
letinbit <letinbit@gmail.com>
2022-12-15 06:27:11 +0300

    Function that print the firat 98 fibonacii numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+/* Author is Neway Solomon */
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * This program prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int count;
+	long int first = 0;
+	long int second = 1;
+	long int third = first + second;
+
+	for (count = 3; count < 101; count++)
+	{
+		if (count == 100)
+			printf("%ld\n", third);
+		else
+			printf("%ld, ", third);
+		first = second;
+		second = third;
+		third = first + second;
+	}
+	return (0);
+}
