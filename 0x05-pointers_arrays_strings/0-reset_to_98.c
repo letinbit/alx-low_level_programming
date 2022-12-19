e90cb422c9b6c92114707caf99dbaa98a672003e
letinbit <letinbit@gmail.com>
2022-12-19 23:03:35 +0300

    Functiona that can reset the value to 98 that a pointer points

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/*Author is Neway Solomon */
+
+/**
+ * reset_to_98 - is a function that resete the value to 98
+ * this fuunction will be called by the main funvtion
+ * @*n: is a pointer that points where the value stored
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
