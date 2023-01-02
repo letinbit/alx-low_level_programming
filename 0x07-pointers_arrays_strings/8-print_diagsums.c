e20b25e8ff69e272d7805f83ded9d0b54b803e94
letinbit <letinbit@gmail.com>
2023-01-03 01:41:00 +0300

    Function that sum diagonals value of an Arra-two dimentional

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+/* Author is Neway Solomon */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsum - function that print two diagonals of two dimentional array
+ * @a: pointer
+ * @size: size of an arry
+ * Return: Nothing
+ */
+void print_diagsum(int *a, int size)
+{
+	int i;
+	int tmpsize;
+	unsigned int bkdiagonal, frdiagonal;
+
+	tmpsize = 0;
+	bkdiagonal = 0;
+	frdiagonal = 0;
+
+	tmpsize = (size * size) - 1;
+	for (i = 0; i <= tmpsize; i = i + (size + 1))
+	{
+		bkdiagonal = bkdiagonal + a[i];
+	}
+	for (i = (size - 1); i < tmpsize; i = i + (size - 1))
+	{
+		frdiagonal = frdiagonal + a[i];
+	}
+	printf("%d, %d\n", bkdiagonal, frdiagonal);
+}
