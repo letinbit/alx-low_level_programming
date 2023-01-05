dafecfbdc1e642fd0f8fa183d68cf681e21b8997
letinbit <letinbit@gmail.com>
2023-01-05 09:06:11 +0300

    function that returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+/* Author is Neway*/
+
+/**
+ * factorial - function that cakculate the fuctorial of n
+ * @n: Its factorial will be calculated
+ * Return: fact. of n if n>0 or error msg
+ */
+int factorial(int n)
+{
+	int result = 1;
+	int i = 1;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+	else if (n > 1)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			result = result * i;
+		}
+	}
+	return (result);
+}
