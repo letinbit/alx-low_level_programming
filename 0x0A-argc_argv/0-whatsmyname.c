2ca1e8bbe548a843ada257cb8c1dad9fb8c81fd6
letinbit <letinbit@gmail.com>
2023-01-11 18:26:39 +0300

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+/* Author is Neway */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that prints the name of the program
+ * @argc: parameter to count
+ * @argv: pointer parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
