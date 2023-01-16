d8c9cccfe6317a3e39b5a5d12a05babf693d10e5
letinbit <letinbit@gmail.com>
2023-01-16 19:29:56 +0300

    struct dog with the following elements:name, type = char * age, type = float owner, type = char *

--- 0x0E. C - Structures, typedef/dog.h
+++ 0x0E. C - Structures, typedef/dog.h
@@ -1,4 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - structure called dog
+ * @name: character type pointer
+ * @age: float type character
+ * @owner: charater type owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+ 
 #endif
