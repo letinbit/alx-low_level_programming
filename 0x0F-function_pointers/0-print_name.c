fcba2e5811858130831ac9a8c3316a98160edb08
letinbit <letinbit@gmail.com>
2023-01-18 19:36:54 +0300

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
- * print_name - function that prints the name
- * - function that has function pointer as arument
+ * print_name - function that has function pointer as argemn to print character
  * @name: character type pointer to name
  * @f: a pointer to function
  *
@@ -10,7 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	(*f)(name);
 }
