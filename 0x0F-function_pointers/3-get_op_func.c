dcfb66afe8dfa3433eb0cb1fa0f079239f7bf0e8
letinbit <letinbit@gmail.com>
2023-01-18 18:24:09 +0300

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * get_op_func - Act as calculator
+ * @s: operator
+ *
+ * Return: function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (*ops[i].op == *s && s[1] == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * op_add - function that add 2 operands.
+ * @a: first operand
+ * @b: second operator
+ *
+ * Return: Addition result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that calculate difference of two operands
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that multiply two numbers.
+ * @a: first operand
+ * @b: second operand
+ * Return: multiplication result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that perform division of a by b
+ * @a: devidened
+ * @b: devisionr
+ *
+ * Return: the division of both numbers.
+ */
+int op_div(int a, int b)
+{
+	if (b <= 0 || b > a)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - function that find the remender if a devided by b
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	if (b <= 0 || b > a)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
